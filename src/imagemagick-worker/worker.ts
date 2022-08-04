6a07abd6ed7b4f146823d8966a2d747704a37fd3
Armaan A <armaanaggarwal@gmail.com>
2022-08-03 20:45:34 -0700

    greatly simplify worker.ts

--- src/imagemagick-worker/worker.ts
+++ src/imagemagick-worker/worker.ts
@@ -49,29 +49,19 @@ export const convertFile = async (data: ConvertFile) => {
   }
 
   else {
-    // This monstrosity of promises is for promisifying two functions:
-    // One is the ImageMagick.read function, where modifications are applied,
-    // and the other being the one where we get the Uint8Array data
-    return await new Promise<string>((fulfilled) => {
-      ImageMagick.read(data.content, async (image) => {
-        // If we are converting to JPG, turn off alpha channel We are doing this
-        // explictely because for some reason, ImageMagick corrupts the background
-        if (data.convertTo.toLowerCase() === "jpg") {
-          // image.alpha(AlphaOption.Remove);
-        }
+    let image = MagickImage.create();
+    image.read(data.content);
 
-        const newData = await new Promise<Uint8Array>((fulfilled) => {
-          // @ts-ignore
-          image.write((newData) => fulfilled(newData), data.convertTo);
-        });
-        
-        // We cannot return a Uint8Array from a web worker
-        // so we convert the data to a Base64 string and then on 
-        // the main thread, we convert it back for downloading
-        const stringData = bytesToBase64(newData)
-        fulfilled(stringData);
-      });
+    const convertedImage = await new Promise<Uint8Array>((fulfilled) => {
+      // @ts-ignore
+      image.write((newData) => fulfilled(newData), data.convertTo);
     });
+
+    // We cannot return a Uint8Array from a web worker
+    // so we convert the data to a Base64 string and then on 
+    // the main thread, we convert it back for downloading
+    const stringData = bytesToBase64(convertedImage);
+    return stringData;
   }
 };
 
